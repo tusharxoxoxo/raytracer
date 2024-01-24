0188f46ee40fa4317990b062ae1935ef61211e66
Tushar Dahiya <tusharxoxoxo@gmail.com>
2024-01-24 12:59:40 +0530

    damn this is throwing too many errors

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # raytracer
+
+-> we will be shooting rays out from the camera into a scene, and those rays will hit smothing bouce around and hit someother object
+and we will take some color from each of those collisions and determine what color should show up at a given axis
+
+-> this is kind of path tracing because we are following around the path of rays 
+
+-> we will be using the ppm image format, it is a strange image format 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use itertools::Itertools;
+use core::f64;
+use std::{f64, fs, io};
+
+const IMAGE_WIDTH: u32 = 3; 
+const IMAGE_HEIGHT: u32 = 2;
+const MAX_PIXEL_VALUE: u32 = 255;
+
+fn main() -> io::Result<()> {
+    let pixels = (0..IMAGE_HEIGHT)
+        .cartesian_product(0..IMAGE_WIDTH)
+        .map(|(y, x)| {
+            let r = x as f64 (IMAGE_WIDTH - 1) as f64;
+            let g = y as f64 (IMAGE_HEIGHT - 1) as f64;
+            let b = 0.0;
+            format!(
+                "{} {} {}\n", 
+                r * 255.0, 
+                g * 255.0, 
+                b * 255.0
+                )
+        })
+    .chuncks(IMAGE_WIDTH as usize)
+        .into_iter()
+        .map(|chunk| chunk.collect::<Vec<_>>().join(""))
+        .join("\n");
+
+    print!("{}", pixels);
+    fs::write(
+        "output.ppm",
+        format!(
+            "P3\n{} {}\n{}\n",
+            IMAGE_WIDTH,
+            IMAGE_HEIGHT,
+            MAX_PIXEL_VALUE,
+            pixels
+        ),
 }
